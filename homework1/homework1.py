307e89ed01fd187f572c557d8dc6dede478294d1
machblossom <machblossom@berkeley.edu>
2025-09-16 13:03:54 -0700

    done with hw2

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -72,22 +72,178 @@ print(type(n)) # n is a range variable, it generates a sequence of numbers
 print(10 > 9) # True, 10 is greater than 9
 print(10 == 9) # False, 10 is not equivalent to 9
 print(10 <= 9) # False, 10 is not smaller than or equal to 9
-print(bool("abc")) # True, 
-print(bool(123)) # True, 
-print(bool(["apple", "cherry", "banana"])) #True, 
-print(bool(True)) # True, 
-print(bool(False)) # False, 
-print(bool(0)) # False, 
-print(bool("")) # False, 
-print(bool(" ")) # True, 
-print(bool(())) # False,
-print(bool([])) # False,
-print(bool({})) # False,
-bool(True and False)
-bool(True and True)
-bool(False and False)
-bool(True or False)
-bool(True or True)
-bool(False or False)
-bool(not(False))
-bool(not(True))
+print(bool("abc")) # True, contains a string (is not empty)
+print(bool(123)) # True, contains numbers (is not empty)
+print(bool(["apple", "cherry", "banana"])) # True, contains words in a list (is not empty)
+print(bool(True)) # True, True truth value returns True
+print(bool(False)) # False, False truth value returns False
+print(bool(0)) # False, contains a zero integer
+print(bool("")) # False, string is empty
+print(bool(" ")) # True, string is not empty (whitespace is not empty)
+print(bool(())) # False, parentheses are empty
+print(bool([])) # False, brackets are empty
+print(bool({})) # False, curly brackets are empty
+print(bool(True and False)) # False, "and" requires both to be True
+print(bool(True and True)) # True, both are true
+print(bool(False and False)) # False, "and" requires both to be True
+print(bool(True or False)) # True, "or" requires at least one to be True
+print(bool(True or True)) # True, "or" requires at least one to be True
+print(bool(False or False)) # False, neither are True
+print(bool(not(False))) # True, not() reverses the truth value of False
+print(bool(not(True))) # False, not() reverses the truth value of True
+
+"""
+1. The expressions returning True contain values or "something" inside of it,
+   like strings or numbers in between things. Those returning False
+   contain nothing, or have the value 0.
+2. The expression result that surprised me was "print(bool(" "))" in that it
+   returned True. Even a spacec in parentheses can be interpreted as being 
+   "something" in between things.
+3. "print(bool([" "]))" Will return True. It contains quotation marks (string)
+   indicating that there is "something" in between the brackets.
+4. "print(bool([ ]))" will return False. Even though there is a space in
+   between the brackets, it isn't inside quotation marks (string) so it
+   will return False.
+"""
+
+# --- Operators --- 
+
+print(10 + 5) # 15, + performs addition
+print(10 - 5) # 5, - performs subtraction
+print(2 * 4) # 8, * performs multiplication
+print(6 / 3) # 2.0, / performs division (leaves as float)
+print(5 % 2) # 1, % performs division (but prints out the remainder)
+print(3 ** 2) # 9, ** performs exponentials
+print(15 // 2) # 7, performs division (does not leave as float (rounds down))
+
+print(5 == 2) # False, == returns truth value stating if A is equivalent to B
+print(10 != 10) # False, != returns truth value stating if A isn't equivalent to B
+print(2 < 5) # True, < returns truth value stating if A is smaller than B
+print(12 > 5) # True, > returns truth value stating if A is bigger than B
+print(5 <= 6) # True, <= returns truth value stating if A is smaller or equal to B
+print(1 >= 10) # False, >= returns truth value stating if A is bigger or equal to B
+
+x = 5 
+print(x) 
+x += 5 # 10, adds 5 to original value of x (5 + 5 = 10)
+print(x)
+x -= 4 # 6, subtracts 4 from previous value of x (10 - 4 = 6)
+print(x)
+x *= 3 # 18, multiplies 3 to the previous value of x (6 * 3 = 18)
+print(x)
+
+"""
+1. The logic operator "and" returns a True truth value if all 
+   operands have a True truth value.
+   a = 2
+   b = 5
+   True: print(a > 1 and b < 10)
+   False: print(a > 1 and b > 10)
+2. The logic operator "or" returns a True truth value if at
+   least ONE operand has a True truth value.
+   a = 1
+   b = 2
+   True: print(a == 1 or b == 5)
+   False: print(a == 4 or b == 9)
+3. The logic operator "not" returns the reversed truth value
+   of the expression.
+   True: print(bool(not(False)))
+   False: print(bool(not(True)))
+
+1. "/" leaves the quotient as a float. "//" leaves the quotient 
+   as an integer.
+2. "%" prints out the remainder of a division problem. "//"
+   leaves the quotient as an integer, without the remainder.
+3. When calculating the remainder by dividing two numbers,
+   one must use the "%" operator.
+   print(36 % 5) # Prints out 1
+4. Assignment operators work by assigning values or characters
+   to variables.
+"""
+
+# --- Strings ---
+
+my_string = "hello"
+
+print(my_string) # Prints: hello
+print(my_string[0]) # Prints: h
+print(my_string[1]) # Prints: e
+print(my_string[2]) # Prints: l
+print(my_string[3]) # Prints: l
+print(my_string[4]) # Prints: o
+print(my_string[-1]) # Prints: o
+print(my_string[1:3]) # Prints: el (slicing)
+print(my_string[0:5:2]) # Prints: hlo (slicing)
+print(len(my_string)) # Prints: 5
+my_string += "goodbye"
+print(my_string) # Prints: hellogoodbye
+my_string = "hello"
+print(my_string * 7) # Prints: hellohellohellohellohellohellohello
+
+"""
+1. Slicing returns a new sequences of strings from our original
+   expression. Slicing was done for 8. and 9. 
+2. The result is "Hello, my name is Oski".
+3. The result is "Hello, my name is Oski".
+4. f strings allow for expressions like variables, function, 
+   and operations to be directly embedded within the string
+   by enclosing them in curly braces {}.
+"""
+
+# --- Terminal Commands ---
+
+# cd
+# Changes directories. Use it to move from one folder to another.
+# Example: cd Desktop
+# ls
+# Lists all content in current directory.
+# Example: (while in python_decal_fall25) ls
+# ls -a
+# Lists all content (hidden as well) in current directory.
+# Example: (while in python_decal_fall25) ls -a
+# mkdir
+# Creates a new daughter directory in current (parent) directory.
+# Example: mkdir new_directory
+# cat
+# Prints out the script of code from file.
+# Example: cat test.py
+# pwd
+# Prints out the current working directory.
+# Examples: (while in python_decal_fall25) pwd
+# cd ..
+# Moves up one directory (to parent)
+# Example: cd ..
+# cd .
+# Changes directory to the current directory
+# Example: cd .
+# cd ~
+# Changes directory to the user's home directory
+# Example: cd ~
+# cp
+# Copies files and directories to paste them to others.
+# Example: cp test.py lecture_notes/
+# mv
+# Moves files to directories and can rename them.
+# Example: mv test.py joshua/
+# rm
+# Removes files from a Git repository.
+# Example: rm test.py
+# clear
+# Clears the terminal screen.
+# Example: clear
+# grep
+# Looks for specific content in text files
+# Example: grep "dog" animals.txt
+
+"""
+1. "head" prints out the beginning of a file: head homework1.py
+   "tail" prints out the end of a file: tail homework1.py
+   "history" prints out a list of previously used commands: history
+2. "ls" prints out all content from a directory. "ls -a" prints out
+   all content from a directory, including hidden content.
+3. A hidden file is a file that is not displayed by default in a 
+   file browser.
+4. "-l" the -l flag from ls -l displays a long list of content.
+   "-i" the -i flag from rm -i prompts for confirmation before deletion.
+   "-f" the -f flag from rm -f forces deletion without prompting.
+"""
---
+++ homework2/homework2.py
@@ -0,0 +1,76 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+#   Git is a software that could be used to make changes and updates to files and directories. GitHub is a website 
+#   where you can create repositories for people to see online. Git Bash is a terminal system to use Git comands.
+
+# 2) What’s the difference between the terminal and the command line?
+#   Terminal and command line are usually used synonomously to mean the same thing. Terminals give access to 
+#   the computer's console, but command lines can have different interfaces.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+#   Windows PowerShell differs from Git Bash in that PowerShell is strictly made for Windows devices and used
+#   by them. Git Bash is a command-line that is Unix-based, used on Apple products.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+#   Anaconda is a software that include Python and many, many other applications like Jupyter Notebooks, NumPy, etc.
+#   Python is a programming language used to create applications, softwares, simulations, etc. Conda is within
+#   Anaconda and is used to manage packages and environments.
+
+# 5) What is VS Code? 
+#   VS Code is an application used to create programs in many different coding languages like C++, Python, etc.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+#   Jupyter Notebook is an application used to create and share documents like code. Jupyter Lab works with
+#   Jupyter Notebook but provides a more enhanced experience, working with multiple components in a single
+#   workspace.
+
+# 7) What does ~/ mean?
+#   ~/ refers to the user's root directory. Like ~/Documents
+
+# 8) What’s the difference between an absolute path and a relative path?
+#   An absolute path is the complete and total path from the root. The relative path is dependent on where I am in the directory.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+#   Absolute: /c/Users/joshu/python_decal_fall25/course_assignments/homework2
+#   Relative: ../python_decal_fall25/course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+#   To go from child (course_assignments/homework2/) directory to parent (course_assignments/) directory, you use cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+#   rm ./ removes the current directory.
+
+# 12) What do the following commands do?
+# git add puts files to the staging area.
+# git commit puts these files to my local repository.
+# git push puts these files to my remote repository.
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+#   git add . adds all files to the staging area whereas git add <file> adds <file> to the staging area.
+
+# 14) What do "git status" and "git log -1" do?
+#   git status shows the current state of the local Git repository. git log -1 shows the most recent history.
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+#   Cloning a repository means to copy a repository without editing it whereas pulling from it means to edit 
+#   and update the existing local repository.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+#   I haven't experienced a bug or error that was a hassle to fix. But I did forget to download Python onto my
+#   laptop at first.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+#   N/A
+
+# 18) Tell me a fun fact!
+#   TrES-2b is the darkest exoplanet to date, only reflecting less than 5% (or even less) of light.
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(3 ** 4) # Prints: 81, the ** operator raises A (3) to the power of B (4). 
